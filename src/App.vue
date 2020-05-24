9e89daf2b59675968a88ec100dab4578cd36cb36
Nils Poltmann <nils@poltmann.com>
2020-05-24 10:08:33 +0200

    Added GitHub link

--- src/App.vue
+++ src/App.vue
@@ -2,4 +2,9 @@
     #app
         .container.mx-auto
             router-view
+        .container.mx-auto.text-center.mt-16
+            a(
+                href="https://github.com/poldixd/vue-wasletztepreis"
+                rel="noopener"
+            ) GitHub
 </template>
