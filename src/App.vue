206586509fc63145493b03582ff5b1abcc11de48
Nils Poltmann <nils@poltmann.com>
2020-05-25 11:01:26 +0200

    Added more information

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,18 @@
     #app
         .container.mx-auto
             router-view
-        .container.mx-auto.text-center.mt-16
+        .container.mx-auto.text-center.mt-16.space-x-8
+            a(
+                href="https://www.paypal.com/de/webapps/mpp/paypal-fees"
+                rel="noopener"
+
+            ) PayPal Gebühreninformation
+            span Stand: 18.12.2019
             a(
                 href="https://github.com/poldixd/vue-wasletztepreis"
                 rel="noopener"
             ) GitHub
+
+        .container.mx-auto.text-center.mt-8.text-sm
+            span alle Angaben ohne Gewähr
 </template>
